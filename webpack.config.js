c19bb8a44236060a09299684a8d8f54f3e70c917
Sylphony <symphonictwist@gmail.com>
2016-12-26 01:07:02 -0800

    Added webpack config

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+var webpack = require("webpack");
+
+module.exports = {
+    entry: "./src/app/app.js",
+
+    output: {
+        path: __dirname,
+        filename: "./src/app/build/app-build.js"
+    },
+
+    cache: true,
+    
+    module: {
+        loaders: [
+            { 
+                test: /\.jsx?$/,
+                loader: "babel-loader",
+                exclude: /node_modules/,
+                query: {
+                    presets: ["es2015", "react"]
+                }
+            }
+        ]
+    },
+
+    resolve: {
+        extensions: ["", ".js", ".jsx"]
+    },
+
+    node: {
+        fs: "empty",        // Fixes the node-fs issue
+        __dirname: true     // Sets the true directory path
+    }
+};
